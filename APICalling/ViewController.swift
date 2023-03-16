61ed79029f4f2c50db7c288a2c25020b1edd8575
SnehKalariya <snehkalariya9583@gmail.com>
2023-03-16 20:26:23 +0530

    complete

--- APICalling/ViewController.swift
+++ APICalling/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+struct GetApi: Codable{
+    var userId: Int
+    var id: Int
+    var title: String
+    var body: String
 }
 
+class ViewController: UIViewController {
+
+    var getApiArray: [GetApi] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getData()
+    }
+    
+    func getData() {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
+        var ur = URLRequest(url: url!)
+        ur.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: ur){data,response,error in
+            do{
+                if error == nil {
+                    self.getApiArray = try JSONDecoder().decode([GetApi].self, from: data!)
+                    print(self.getApiArray)
+                }
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+}
